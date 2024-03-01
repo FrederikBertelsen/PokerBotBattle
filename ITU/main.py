7ac523956cc3b8683bb3e4184329935563fa6194
Frederik Bertelsen <frbb@itu.dk>
2024-03-01 22:20:14 +0100

    wad

--- ITU/main.py
+++ ITU/main.py
@@ -20,6 +20,7 @@ import Simply
 import Simply2
 import SimplyJob
 import Dani
+import oddsBot
 from test import run_benchmark_with_analysis, run_benchmark
 import Hercules
 import Hermes
@@ -29,7 +30,7 @@ import nissemand
 import DaniRedux
 import mobot
 
-bot_to_test = DaniRedux
+bot_to_test = oddsBot
 
 # bot_to_test = Hellmuth
 # other_bot_to_test = Hercules
@@ -38,9 +39,9 @@ bot_to_test = DaniRedux
 # super board
 # bots = [example_bot, simple, Hercules, bot_to_test, UnfairAdvantage, GiveMeAJob, Simply, Hermes]
 # high board
-# bots = [Simply, botimus, simple, example_bot, bot_to_test, over_3, EMIL, old_bot, EMIL_pair, fish_bot]
+bots = [Simply, botimus, simple, example_bot, bot_to_test, over_3, EMIL, old_bot, EMIL_pair, fish_bot]
 # mid board
-bots = [bot_to_test, Hermes, old_bot, callBot, over_3, EMIL, nissemand, fish_bot, botimus, bluff_bot]
+# bots = [bot_to_test, Hermes, old_bot, callBot, over_3, EMIL, nissemand, fish_bot, botimus, bluff_bot]
 # low board
 # bots = [bot_to_test, randombot, fish_bot, old_bot, knockoutbot2, bluff_bot, callBot, randombot2, knockoutbot, bluff_bot2]
 
